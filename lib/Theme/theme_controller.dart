e40c7b15794365364cae369df67e146fc98c673e
Rahul Pandey <rahulp@mailinator.com>
2025-03-21 11:53:48 +0530

    solved initial theme bug

--- lib/Theme/theme_controller.dart
+++ lib/Theme/theme_controller.dart
@@ -4,24 +4,28 @@ import 'package:hive/hive.dart';
 
 class ThemeController extends GetxController {
   final _isDarkMode = false.obs;
-  
+
   bool get isDarkMode => _isDarkMode.value;
-  
+
   @override
   void onInit() {
     super.onInit();
     loadThemeFromStorage();
+    // Initialize with system theme
+    _isDarkMode.value = Get.isDarkMode;
   }
 
   Future<void> loadThemeFromStorage() async {
     final box = await Hive.openBox('settings');
-    _isDarkMode.value = box.get('isDarkMode', defaultValue: true);
+    _isDarkMode.value = box.get('isDarkMode', defaultValue: Get.isDarkMode);
   }
 
-  Future<void> toggleTheme() async {
+  void toggleTheme() {
     _isDarkMode.value = !_isDarkMode.value;
-    final box = await Hive.openBox('settings');
-    await box.put('isDarkMode', _isDarkMode.value);
     Get.changeThemeMode(_isDarkMode.value ? ThemeMode.dark : ThemeMode.light);
+    // Save the theme preference asynchronously
+    Hive.openBox('settings').then((box) {
+      box.put('isDarkMode', _isDarkMode.value);
+    });
   }
 }
